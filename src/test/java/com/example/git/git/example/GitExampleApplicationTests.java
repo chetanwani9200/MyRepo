26c0c575a5e8731a7235f3c064f3cfd6b8a300a2
chetanwani9200 <chetanwani9200@gmail.com>
2024-08-12 09:33:01 +0000

    initi commit from cli

---
+++ src/test/java/com/example/git/git/example/GitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git.git.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
